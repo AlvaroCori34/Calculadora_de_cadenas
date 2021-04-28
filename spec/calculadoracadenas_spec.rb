21080965c912d754cfe0dc76621f6b34696962d0
AlvaroCori <bacs200018@gmail.com>
2021-04-28 17:03:12 -0400

    2do ciclo prueba con 1 numero

--- spec/calculadoracadenas_spec.rb
+++ spec/calculadoracadenas_spec.rb
@@ -1,4 +1,13 @@
-RSpec.describe "" do
-    it "" do
+RSpec.describe "Calculadora De Cadenas" do
+    it "Deberia devolver el numero 3 para la cadena 3" do
+        expect(calcularCadena("3")).to eq(3)
     end
+
+    it "Deberia devolver el numero 5 para la cadena 5" do
+        expect(calcularCadena("5")).to eq(5)
+    end
+end
+
+def calcularCadena(cadena)
+    cadena[0].to_i
 end
